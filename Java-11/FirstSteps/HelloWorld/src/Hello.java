6580d1f49fce1759ba11a570724a7725e8032da9
Cameron Whyte <cameron12whyte@gmail.com>
2020-03-07 15:37:13 +0000

    Hello, (Name). Comments to define what things do!

--- Java-11/FirstSteps/HelloWorld/src/Hello.java
+++ Java-11/FirstSteps/HelloWorld/src/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
 
+    /*
+    Main method - Java will always look for the main method.
+    Curly Brackets {} - These define the CODE BLOCKS.
+     */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, Cameron"); //Code in methods are called statements. Expressions are the parameters in a statement.
     }
 }
