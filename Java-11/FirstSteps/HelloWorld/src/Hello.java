30bad2fe2639be607fe7c38813a09b6d6661860b
Cameron Whyte <cameron12whyte@gmail.com>
2020-03-07 15:46:50 +0000

    Declaration statements

--- Java-11/FirstSteps/HelloWorld/src/Hello.java
+++ Java-11/FirstSteps/HelloWorld/src/Hello.java
@@ -5,6 +5,9 @@ public class Hello {
     Curly Brackets {} - These define the CODE BLOCKS.
      */
     public static void main(String[] args) {
-        System.out.println("Hello, Cameron"); //Code in methods are called statements. Expressions are the parameters in a statement.
+        System.out.println("Hello, Cameron"); //Code in methods are called statements. Expressions are the constructs that evaluates to a single value, these generally go in the method parameters.
+
+        int myFirstNumber = (10+5)+(2*10); //Declaration statement, Declaration statements are to define a variable by indicating the data type. The data type here is an int, the int is short for integer and it can only hold numbers.
+        System.out.println(myFirstNumber); //Outputs the value held by the variable.
     }
 }
