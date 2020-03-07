3c9e51e7fbf56bd72ab4439a3300014005b7713c
Cameron Whyte <cameron12whyte@gmail.com>
2020-03-07 16:03:00 +0000

    Extra declaration statements.

--- Java-11/FirstSteps/HelloWorld/src/Hello.java
+++ Java-11/FirstSteps/HelloWorld/src/Hello.java
@@ -7,7 +7,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Cameron"); //Code in methods are called statements. Expressions are the constructs that evaluates to a single value, these generally go in the method parameters.
 
-        int myFirstNumber = (10+5)+(2*10); //Declaration statement, Declaration statements are to define a variable by indicating the data type. The data type here is an int, the int is short for integer and it can only hold numbers.
-        System.out.println(myFirstNumber); //Outputs the value held by the variable.
+        int myFirstNumber = (10 + 5) + (2 * 10); //Declaration statement, Declaration statements are to define a variable by indicating the data type. The data type here is an int, the int is short for integer and it can only hold numbers.
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal); //Outputs the value held by the variable.
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
     }
 }
