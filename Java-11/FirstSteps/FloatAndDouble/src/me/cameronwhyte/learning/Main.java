bd2f27393442a2bfef2004e54a5a8b28cb8f1b39
Cameron Whyte <cameron12whyte@gmail.com>
2020-03-08 13:49:17 +0000

    Extra floats and doubles

--- Java-11/FirstSteps/FloatAndDouble/src/me/cameronwhyte/learning/Main.java
+++ Java-11/FirstSteps/FloatAndDouble/src/me/cameronwhyte/learning/Main.java
@@ -2,6 +2,14 @@ package me.cameronwhyte.learning;
 
 public class Main {
 
+    /*
+    Float and Double are great for general floating point operations.
+    But both are not great to use where precise calculations are required.
+    This is due to a limitation with how floating point numbers are stored.
+    This is not a Java problem though.
+
+    Java has a class called BigDecimal that overcomes this limitation.
+     */
     public static void main(String[] args) {
 
         float myMinFloatValue = Float.MIN_VALUE;
@@ -14,8 +22,20 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
-        float myFloatValue = (float) 5.25;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        double pounds = 200d;
+        double kilograms = pounds*.45359237D;
+        System.out.println("Converted kilograms = " + kilograms);
+
+        double pi = 3.1415927D;
+        double anotherNumber = 3_000_000.4_567_890d;
+        System.out.println("Pi = " + pi);
+        System.out.println("Another number = " + anotherNumber);
     }
 }
