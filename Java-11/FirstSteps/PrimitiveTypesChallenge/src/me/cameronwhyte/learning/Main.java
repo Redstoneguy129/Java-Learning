dcdedd7d9bd8150cad1bbc87b42561d44ab64069
Cameron Whyte <cameron12whyte@gmail.com>
2020-03-08 13:06:29 +0000

    Primitive Types

---
+++ Java-11/FirstSteps/PrimitiveTypesChallenge/src/me/cameronwhyte/learning/Main.java
@@ -0,0 +1,17 @@
+package me.cameronwhyte.learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+        short shortTotal = (short) (1000 + 10 *
+                (byteValue + shortValue + intValue)); //You can use the same statement over multiple lines.
+    }
+}
